ffd5e164a319ac35d365c6867e59beaecbcb7cfe
Sunidhichaudhary21 <csunidhi22@gmail.com>
2026-05-05 20:22:05 +0530

    modified credits point

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,11 +7,13 @@ import {
   XIcon,
 } from "lucide-react";
 import { GhostButton, PrimaryButton } from "./Buttons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useClerk, useUser, UserButton } from "@clerk/react";
+import { useClerk, useUser, UserButton, useAuth } from "@clerk/react";
+import api from "../configs/axios";
+import toast from "react-hot-toast";
 
 
 export default function Navbar() {
@@ -21,12 +23,39 @@ export default function Navbar() {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const [credits,setCredits]=useState(0);
+  const {pathname}=useLocation()
+  const {getToken} = useAuth()
+
   const navLinks = [
     { name: "Home", href: "/#" },
     { name: "Create", href: "/generate" },
     { name: "Community", href: "/community" },
     { name: "Plans", href: "/plans" },
   ];
+
+  const getUserCredits = async()=>{
+    try{
+      const token =await getToken()
+      const {data} =await api.get('/api/user/credits',{headers:
+        {Authorization:`Bearer ${token}`}})
+        setCredits(data.credits)
+
+
+    }catch(error:any){
+      toast.error(error?.response?.data?.message|| error.message)
+      console.log(error);
+
+
+    }
+  }
+  useEffect(()=>{
+    if(user){
+      (async()=>await getUserCredits())();
+    }
+  },[user, pathname])
+
+
 
   return (
     <motion.nav
@@ -75,7 +104,7 @@ export default function Navbar() {
               onClick={() => navigate("/")}
               className="border-none text-gray-300 sm:py-1.5"
             >
-              Credits:
+              Credits:{credits}
             </GhostButton>
             <UserButton>
               <UserButton.MenuItems>
